cfd77b95934dc696de92b2c401c1846dce374617
danmt <danielarturomt@gmail.com>
2020-12-04 10:46:45 -0300

    add a concat unique operator

--- operators.ts
+++ operators.ts
@@ -1,7 +1,21 @@
-import { filter } from "rxjs/operators";
+import { from } from "rxjs";
+import { concatMap, distinct, filter, last, toArray } from "rxjs/operators";
 
 export const all = <T>(...conditions: ((value: T) => boolean)[]) =>
   filter((value: T) => conditions.every(condition => condition(value)));
 
 export const any = <T>(...conditions: ((value: T) => boolean)[]) =>
   filter((value: T) => conditions.some(condition => condition(value)));
+
+export const concatUnique = <T, U>(keySelector?: (value: T) => U) => (
+  arrays: T[][]
+) =>
+  from(arrays).pipe(
+    concatMap(array =>
+      from(array).pipe(
+        distinct<T, U>(keySelector),
+        toArray()
+      )
+    ),
+    last()
+  );
